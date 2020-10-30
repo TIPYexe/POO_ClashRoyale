e2fb14496a35279ddbbe0cf1269896faed65ea97
Tipy <53595545+TIPYexe@users.noreply.github.com>
2020-10-30 22:11:40 +0200

    Update ConsoleApplication1.cpp

--- Clasele separate/ConsoleApplication1.cpp
+++ Clasele separate/ConsoleApplication1.cpp
@@ -7,25 +7,6 @@
 #include "Harta.h"
 
 
-/*
-void afisare_camp(char field[], Personaj& Enemy, Personaj& Friend)
-{
-	while (Enemy.get_hp() > 0 && Friend.get_hp() > 0)
-	{
-		system("CLS");
-		strcpy_s(field, 42, "........................................."); //resetez toate pozitiile cu "."
-		field[Enemy.get_locatie() - 1] = Enemy.get_name()[0]; //pun initialele numelor pesonajelor acolo unde trebuie
-		field[Friend.get_locatie() - 1] = Friend.get_name()[0];
-		cout << field << '\n'; // afisez campul de lupta
-
-		printf("\n");
-		stats(Enemy, Friend); // afisez si statistici despre personaje
-
-		sleep_for(chrono::milliseconds(17)); // face jocul sa fie in 60FPS, si se vede mai bine ca personajele 
-											 // isi dau atacurile pe rand, deci functioneaza delay-ul
-	}
-}
-*/
 
 int main() {
 
@@ -43,7 +24,6 @@ int main() {
 	Turn Prieten(42);
 
 	Harta Field;
-	//char field[] = ".........................................";
 
 	srand(time(NULL));
 	int nr = rand() % 4;
@@ -57,14 +37,14 @@ int main() {
 
 	// fiecare meteoda si functia de afisare a hartii+stats ruleaza pe cate un thread
 	// pentru ca toate aceste functii trebuie sa ruleze simultan
-	thread enemy(&Personaj::automat, ref(Enemy), ref(Friendly));     // am pasat obiectele ca referinte pentru ca altfel
-	thread friendly(&Personaj::automat, ref(Friendly), ref(Enemy));  // nu se modificau valorile, si deci nu aveau relevanta 
-	thread field_t(&Harta::afisare_camp, ref(Enemy), ref(Friendly)); // nici harta nici statisticile, ramaneau neschimbate
+	std::thread enemy(&Personaj::automat, std::ref(Enemy), std::ref(Friendly));     // am pasat obiectele ca referinte pentru ca altfel
+	std::thread friendly(&Personaj::automat, std::ref(Friendly), std::ref(Enemy));  // nu se modificau valorile, si deci nu aveau relevanta 
+	std::thread field_t(&Harta::afisare_camp, std::ref(Enemy), std::ref(Friendly)); // nici harta nici statisticile, ramaneau neschimbate
 
 	enemy.join();
 	friendly.join();
 	field_t.join();
-
+	
 
 	// la finalul jocului
 	system("CLS");
@@ -75,6 +55,6 @@ int main() {
 	else
 		printf("EGALITATE!\n");
 	printf("\n");
-	stats(Enemy, Friendly);
+	Field.stats(Enemy, Friendly);
 
 }
