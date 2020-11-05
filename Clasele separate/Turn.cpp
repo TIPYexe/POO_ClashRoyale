faf96592113614c6a829fb77123cf7dff6c47b5c
Tipy <53595545+TIPYexe@users.noreply.github.com>
2020-11-05 17:18:10 +0200

    Update Turn.cpp

--- Clasele separate/Turn.cpp
+++ Clasele separate/Turn.cpp
@@ -1,9 +1,11 @@
 #include "Turn.h"
 
 void Turn::automat(Personaj& Enemy) {
-    while (abs(Enemy.get_locatie() - locatie) <= range)
+    while (hp > 0 && Enemy.get_hp() > 0)
+        if(abs(Enemy.get_locatie() - locatie) <= range)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(hitspeed));
+        if(hp>0)
         Enemy.get_hit(damage);
     }
 }
