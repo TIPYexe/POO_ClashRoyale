0240297b90e2537877dfb2cc5316155eaf677516
TIPYexe <tipy_serios@yahoo.com>
2020-12-07 22:08:51 +0200

    Update

---
+++ Turn.h
@@ -0,0 +1,52 @@
+#ifndef TURN_H
+#define TURN_H
+
+#include <chrono>
+#include <thread>
+
+#include "Personaj.h"
+
+class Personaj;
+
+class Turn
+{
+private:
+
+	//friend class Personaj;
+
+	int hp;
+	int range;
+	int damage;
+	int hitspeed;
+	int locatie; // ori e 0 ori e 42
+
+public:
+
+	Turn(int in_locatie) {
+		hp = 1000;
+		range = 7;
+		damage = 99;
+		hitspeed = 800;
+		locatie = in_locatie;
+	}
+
+	int get_damage() {
+		return damage;
+	}
+
+	int get_hp() {
+		return hp;
+	}
+
+	int get_locatie() {
+		return locatie;
+	}
+
+	void get_hit(int damage_primit) {
+		hp -= damage_primit;
+	}
+
+	void automat(Personaj& Enemy);
+};
+
+#endif // !TURN_H
